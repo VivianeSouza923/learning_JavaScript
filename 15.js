9e9c5907e58824e63571d189088cee73ccfba6ba
VivianeSouza923 <viviane.souza05@aluno.ifce.edu.br>
2021-11-13 17:03:24 -0300

    update

--- 15.js
+++ 15.js
@@ -1,1 +1,3 @@
-// aula 15 - console log
+// aula 15 - console log - manda mensagem sem incomodar como o alert
+
+console.log("vivi");
---
+++ 16.js
@@ -0,0 +1,23 @@
+// aula 16 - funções - são blocos de códigos que serão executados somente se forem chamados
+// cria-se com o nome function
+
+/*function soma(a, b) {
+    console.log(a + b);
+}
+// chamando a função
+soma(2, 2);*/
+
+//criar retorno de dados
+
+/*function sub(a, b) {
+    return a - b;
+}
+
+var s = sub(5, 3);
+console.log(s);*/
+
+function mul(a, b) {
+    return a * b;
+}
+
+console.log(mul(5, 2));
