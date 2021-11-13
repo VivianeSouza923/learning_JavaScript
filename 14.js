9bca31868d74696793ff3e0e2d4b572b6de86590
VivianeSouza923 <viviane.souza05@aluno.ifce.edu.br>
2021-11-13 16:45:23 -0300

    update

---
+++ 14.js
@@ -0,0 +1,16 @@
+// aula 14 - Arrays
+
+var numero = 6;
+var decimal = 4.5;
+var texto = "Olá mundo";
+var lista = ["laranja", "maçã", "banana", 1234];
+
+alert(lista[3]);
+
+// agora se eu quiser pegar todos os elemetnos:
+
+// para cada i dentro da lista, eu imprimo a posição i dessa lista
+for (i in lista) {
+
+    alert(lista[i]);
+}
