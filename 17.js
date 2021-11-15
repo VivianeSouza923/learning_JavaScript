e8e5068cda2fd6ee98e1c3d9b5b9de4fb2c36aba
VivianeSouza923 <viviane.souza05@aluno.ifce.edu.br>
2021-11-14 23:15:31 -0300

    update

---
+++ 17.js
@@ -0,0 +1,5 @@
+// aula 17 - Eventos- formas de interação da página HTML com o script
+// DEPOIS PESQUISA MAIS EVENTOS EM JAVASCRIPT
+function mensagem(nome) {
+    alert("pare, " + nome);
+}
---
+++ 18.js
@@ -0,0 +1,9 @@
+// aula 18 - DOM
+
+
+function mudaCor(cor) {
+    var elemento = document.getElementById("mensagem")
+    elemento.style.color = cor;
+    elemento.style.backgroundColor = cor;
+    elemento.style.fontSize = "50px";
+}
