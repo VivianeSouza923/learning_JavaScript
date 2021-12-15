a858380ef447adc148a2419a75f76dbca21a1ac7
VivianeSouza923 <viviane.souza05@aluno.ifce.edu.br>
2021-12-15 13:05:39 -0300

    update

---
+++ 31.js
@@ -0,0 +1,24 @@
+// Slideshow - aula 31
+
+$(function() {
+    $('#azul').click(function() {
+        $('p')
+            .css("background-color", "blue")
+            .fadeOut()
+            .delay(1000)
+            .fadeIn();
+    });
+    $('#vermelho').click(function() {
+        $('p').css("background-color", "red");
+        $('#mensagem')
+            .text("Cor alterada com sucesso")
+            .css({
+                color: 'red',
+                border: '1px solid red'
+            })
+            .delay(3000)
+            .addClass('green')
+        $('button').removeClass('red');
+    });
+
+});
