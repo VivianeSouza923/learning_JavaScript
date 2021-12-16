faebe43fff37fc6f7ceabf4c00035c964aaabede
VivianeSouza923 <viviane.souza05@aluno.ifce.edu.br>
2021-12-15 21:26:45 -0300

    update

--- 31.js
+++ 31.js
@@ -1,24 +1,37 @@
 // Slideshow - aula 31
 
 $(function() {
-    $('#azul').click(function() {
-        $('p')
-            .css("background-color", "blue")
-            .fadeOut()
-            .delay(1000)
-            .fadeIn();
+    $('#l1').click(function() {
+        $('#i1').show();
+        $('#i2').hide();
+        $('#i3').hide();
+        $('#i4').hide();
     });
-    $('#vermelho').click(function() {
-        $('p').css("background-color", "red");
-        $('#mensagem')
-            .text("Cor alterada com sucesso")
-            .css({
-                color: 'red',
-                border: '1px solid red'
-            })
-            .delay(3000)
-            .addClass('green')
-        $('button').removeClass('red');
+
+    $('#l2').click(function() {
+        $('#i1').hide();
+        $('#i2').show();
+        $('#i3').hide();
+        $('#i4').hide();
+
     });
+
+    $('#l3').click(function() {
+        $('#i1').hide();
+        $('#i2').hide();
+        $('#i3').show();
+        $('#i4').hide();
+
+    });
+
+    $('#l4').click(function() {
+        $('#i1').hide();
+        $('#i2').hide();
+        $('#i3').hide();
+        $('#i4').show();
+
+    });
+
+
 
 });
